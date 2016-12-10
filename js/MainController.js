718addbfff8f597d4163d80f0f9c3b6ec17ae220
Danielle O'Sullivan <danielleos@users.noreply.github.com>
2016-12-10 22:56:22 +0000

    Update MainController.js

--- js/MainController.js
+++ js/MainController.js
@@ -1,4 +1,13 @@
-app.controller('MainController', ['$scope', function($scope) { 
-	$scope.title = 'Moodslider'; 
-	$scope.promo = '...';
+angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('device', 'img/icons/sets/device-icons.svg', 24);
+  })
+.controller('MainController', function($scope) {
+
+	$scope.mood1 = 85;
+	$scope.mood2 = 40;
+	$scope.mood3 = 25;
+	$scope.mood4 = 75;
+
 });
